6e18ef6b7f5637c394980a7960ad2b63b0d8d7f4
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-03-15 15:29:33 -0600

    Ejercicio 19 Marco Motta #0909-23-2731

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int saldo_inicial = 1000, opc;
+float extra, saldo, retiro;
+
+cout << "\tBienvenido a su cajero virtual"<<endl;
+cout << "1. ingresar dinero en cuenta"<<endl;
+cout << "2.Retirar dinero de la cuenta"<<endl;
+cout << "3. Salir"<<endl;
+cout << "opcion:";
+cin >> opc;
+
+    switch (opc) {
+
+        case 1:
+            cout << "digite la cantida de dinero a ingresar";
+            cin>>extra;
+            saldo = saldo_inicial + extra;
+            cout << "Dinero en cuenta: " <<saldo;break;
+
+        case 2:
+            cout << "digita la cantida de dinero que va a retirar: ";
+            cin >> retiro;
+
+            if (retiro > saldo_inicial){
+                cout << "no tiene esa cantidad de dinero";
+            }else {
+                saldo = saldo_inicial - retiro;
+                cout << "cuanto dinero tienes en tu cuenta: " <<  saldo;
+            }
+        case 3: break;
+    }
+
+    return 0;
+}
