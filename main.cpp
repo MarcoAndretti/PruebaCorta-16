8c71fdd09592b3ab6e7408ffa24037609ed31987
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-03-15 20:16:37 -0600

    Ejercicio 33 Marco Motta 0909-23-2731

--- main.cpp
+++ main.cpp
@@ -223,7 +223,7 @@ int main (){
 
     getch();
     return 0;
-}*/
+}
 
 #include <iostream>
 #include <conio.h>
@@ -244,7 +244,36 @@ int main (){
     }
     getch();
     return 0;
+}*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main (){
+    int numeros [100],n,mayor=0;
+
+    cout<< "digite un numero: ";
+    cin>> n;
+
+    for (int i = 0; i < n; i++) {
+        cout<< i+1 << "digite un numero: ";
+        cin>>numeros[i];
+
+        if (numeros [i] > mayor){
+            mayor = numeros [i];
+        }
+
+    }
+
+    cout << "\n El mayor elemento del vector es: "<<mayor<<endl;
+
+    getch();
+    return 0;
 }
+
+
 
 
 
