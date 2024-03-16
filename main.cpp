ac150e593cd6433f4a3305c11f388fa0455f7832
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-03-15 20:24:53 -0600

    Ejercicio 34 Marco Motta 0909-23-2731

--- main.cpp
+++ main.cpp
@@ -244,7 +244,7 @@ int main (){
     }
     getch();
     return 0;
-}*/
+}
 
 #include <iostream>
 #include <conio.h>
@@ -270,6 +270,31 @@ int main (){
     cout << "\n El mayor elemento del vector es: "<<mayor<<endl;
 
     getch();
+    return 0;
+}*/
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    char letras1 [] = {'a','b','c','d','e'};
+    char letras2 [] = {'f','g','h','i','j'};
+    char letras3 [10];
+
+    for (int i = 0; i < 5; ++i) {
+        letras3 [i] = letras1 [i];
+    }
+
+    for (int i = 5; i < 10 ; ++i) {
+        letras3[i] = letras2 [i-5];
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        cout << letras3[i]<<endl;
+    }
+
     return 0;
 }
 
